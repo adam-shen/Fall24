c8a99b3f100ffc08caeb6b89ffff26d72d482d00
SmallSheep <147547342+SmallSheep993@users.noreply.github.com>
2024-09-30 19:07:43 +0000

    Adding files from SSH

---
+++ CS214/Project I: My Little malloc() /mymalloc.c
@@ -0,0 +1,20 @@
+#include "mymalloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void *mymalloc(size_t size, char *file, int line)
+{
+    return nullptr;
+}
+
+void myfree(void *ptr, char *file, int line)
+{
+
+}
+
+void leak_check()
+{
+
+}

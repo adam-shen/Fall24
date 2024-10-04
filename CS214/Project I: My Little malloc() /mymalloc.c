554133f45cdd5c5375ccba49e8e61bdb50d580f4
adam-shen <113874468+adam-shen@users.noreply.github.com>
2024-10-04 15:04:05 +0000

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	modified:   CS214/Project I: My Little malloc() /mymalloc.c 	modified:   CS214/Project I: My Little malloc() /mymalloc.h

--- CS214/Project I: My Little malloc() /mymalloc.c
+++ CS214/Project I: My Little malloc() /mymalloc.c
@@ -1,18 +1,22 @@
-#include "mymalloc.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "mymalloc.h"
 
 
 
 #define MEMSIZE 4096 // 4KB
 
+static union{ 
+    char bytes [MEMSIZE];
+    double not_used; //makes sure the array is aligned at addresses divisible by 8
+} heap;
 
 void *mymalloc(size_t size, char *file, int line)
 {
 
 
-    return nullptr;
+    return NULL;
 }
 
 void myfree(void *ptr, char *file, int line)
@@ -22,5 +26,17 @@ void myfree(void *ptr, char *file, int line)
 
 void leak_check()
 {
+    
 
 }
+
+static void intialize(void) {
+    
+    atexit(lead_check());
+    
+}
+
+static void print_heap(void) {
+    
+}
+
--- CS214/Project I: My Little malloc() /mymalloc.h
+++ CS214/Project I: My Little malloc() /mymalloc.h
@@ -6,8 +6,8 @@
 
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
-void leak_checker(void);
+void leak_check(void);
 void print_heap(void);
-void intialized(void);
+void intialize(void);
 
 #endif

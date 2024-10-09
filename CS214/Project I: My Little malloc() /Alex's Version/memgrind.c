1e7f6ce230eb23acd71cd6c830a7b50870c7a2a1
adam-shen <113874468+adam-shen@users.noreply.github.com>
2024-10-09 20:16:08 +0000

    deleted:    CS214/El Mos3da/memgrind.c 	deleted:    CS214/El Mos3da/mymalloc.c 	deleted:    CS214/El Mos3da/mymalloc.h 	modified:   CS214/Project I: My Little malloc() /Alex's Version/memgrind 	modified:   CS214/Project I: My Little malloc() /Alex's Version/memgrind.c 	modified:   CS214/Project I: My Little malloc() /Alex's Version/memgrind.o

--- CS214/Project I: My Little malloc() /Alex's Version/memgrind.c
+++ CS214/Project I: My Little malloc() /Alex's Version/memgrind.c
@@ -25,6 +25,7 @@ void task2() {
         myfree(ptrs[i], __FILE__, __LINE__);
     }
 }
+
 // Task 3: Allocate and free 240 times randomly
 void task3() {
     char *ptrs[120];
@@ -56,7 +57,6 @@ void task3() {
             ptrs[index] = ptrs[--allocated];
             ptrs[allocated] = NULL;  // Set the last pointer to NULL
         }
-        //printf("Working here %d\n", i);
     }
 
     // Deallocate any remaining allocated objects
@@ -66,6 +66,72 @@ void task3() {
             ptrs[i] = NULL;  // Set the pointer to NULL after freeing
         }
     }
+}
+
+// Task 4: Allocate and deallocate objects of increasing sizes
+void task4() {
+    char *ptrs[120];
+
+    // Allocate objects of increasing size (from 1 byte to 120 bytes)
+    for (int i = 0; i < 120; i++) {
+        ptrs[i] = (char *)mymalloc(i + 1, __FILE__, __LINE__);  // Allocate i+1 bytes
+        if (ptrs[i] == NULL) {
+            printf("Unable to allocate object %d\n", i);
+            return;
+        }
+    }
+
+    // Deallocate all objects
+    for (int i = 0; i < 120; i++) {
+        myfree(ptrs[i], __FILE__, __LINE__);
+    }
+
+    printf("Task 4 completed: Allocated and deallocated objects of increasing size.\n");
+}
+
+// Task 5: Fragmentation test with random allocations and frees
+void task5() {
+    char *ptrs[120];
+    int allocated = 0;
+
+    // Randomly allocate objects of size between 1 and 120 bytes
+    for (int i = 0; i < 120; i++) {
+        int size = (rand() % 120) + 1;  // Random size between 1 and 120 bytes
+        ptrs[allocated] = (char *)mymalloc(size, __FILE__, __LINE__);
+        if (ptrs[allocated] == NULL) {
+            printf("Unable to allocate object %d of size %d\n", i, size);
+            return;
+        }
+        allocated++;
+    }
+
+    // Randomly free half of the allocated objects
+    for (int i = 0; i < 60; i++) {
+        int index = rand() % allocated;
+        if (ptrs[index] != NULL) {
+            myfree(ptrs[index], __FILE__, __LINE__);
+            ptrs[index] = NULL;
+        }
+    }
+
+    // Allocate new objects after fragmentation
+    for (int i = 0; i < 60; i++) {
+        int size = (rand() % 120) + 1;  // Random size between 1 and 120 bytes
+        ptrs[i] = (char *)mymalloc(size, __FILE__, __LINE__);
+        if (ptrs[i] == NULL) {
+            printf("Unable to allocate object %d after fragmentation\n", i);
+            return;
+        }
+    }
+
+    // Free all remaining objects
+    for (int i = 0; i < 120; i++) {
+        if (ptrs[i] != NULL) {
+            myfree(ptrs[i], __FILE__, __LINE__);
+        }
+    }
+
+    printf("Task 5 completed: Fragmentation test with random allocations and frees.\n");
 }
 
 // Main method
@@ -104,6 +170,28 @@ int main() {
         total_time += calculate_time(start, end);
     }
     printf("Task 3 average time: %ld microseconds\n", total_time / 50);
+
+    // Task 4
+    total_time = 0;
+    printf("task 4 is starting\n");
+    for (int i = 0; i < 50; i++) {
+        gettimeofday(&start, NULL);
+        task4();
+        gettimeofday(&end, NULL);
+        total_time += calculate_time(start, end);
+    }
+    printf("Task 4 average time: %ld microseconds\n", total_time / 50);
+
+    // Task 5
+    total_time = 0;
+    printf("task 5 is starting\n");
+    for (int i = 0; i < 50; i++) {
+        gettimeofday(&start, NULL);
+        task5();
+        gettimeofday(&end, NULL);
+        total_time += calculate_time(start, end);
+    }
+    printf("Task 5 average time: %ld microseconds\n", total_time / 50);
 
     return 0;
 }
